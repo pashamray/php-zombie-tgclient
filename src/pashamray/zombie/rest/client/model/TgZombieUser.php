67f4251cf6e1f6c95a659295d0fab352d273bc16
dev5 <dev5@convidev.pl>
2018-01-23 18:55:36 +0200

    del set get method

--- src/pashamray/zombie/rest/client/model/TgZombieUser.php
+++ src/pashamray/zombie/rest/client/model/TgZombieUser.php
@@ -10,118 +10,22 @@ namespace pashamray\zombie\rest\client\model;
 
 class TgZombieUser
 {
-    private $id;
-    private $access_hash;
-    private $phone;
-    private $first_name;
-    private $last_name;
-    private $username;
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAccessHash()
-    {
-        return $this->access_hash;
-    }
-
-    /**
-     * @param mixed $access_hash
-     */
-    public function setAccessHash($access_hash)
-    {
-        $this->access_hash = $access_hash;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone()
-    {
-        return $this->phone;
-    }
-
-    /**
-     * @param mixed $phone
-     */
-    public function setPhone($phone)
-    {
-        $this->phone = $phone;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getFirstName()
-    {
-        return $this->first_name;
-    }
-
-    /**
-     * @param mixed $first_name
-     */
-    public function setFirstName($first_name)
-    {
-        $this->first_name = $first_name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLastName()
-    {
-        return $this->last_name;
-    }
-
-    /**
-     * @param mixed $last_name
-     */
-    public function setLastName($last_name)
-    {
-        $this->last_name = $last_name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * @param mixed $username
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    }
+    public $id;
+    public $access_hash;
+    public $phone;
+    public $first_name;
+    public $last_name;
+    public $username;
 
     static function fromJson($json)
     {
         $account = new self();
-        $account->setAccessHash($json->access_hash);
-        $account->setId($json->id);
-        $account->setFirstName($json->first_name);
-        $account->setLastName($json->last_name);
-        $account->setUsername($json->username);
-        $account->setPhone($json->phone);
+        $account->access_hash = $json->access_hash;
+        $account->id = $json->id;
+        $account->first_name = $json->first_name;
+        $account->last_name = $json->last_name;
+        $account->username = $json->username;
+        $account->phone = $json->phone;
         return $account;
     }
 }
